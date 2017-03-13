56d64e9728a854e4abf408986e0b0da633e7e0ae
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-03-13 20:57:14 +0300

    Renaming and minor refactoring.

--- src/Customer.java
+++ src/Customer.java
@@ -20,27 +20,27 @@ class Customer {
 
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
+        int totalPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : rentals) {
             double amount = getPriceFor(rental);
 
             // add frequent renter points
-            frequentRenterPoints++;
+            int points = 1;
             // add bonus for a two day new release rental
             if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
                     && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+                points++;
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t"
                     + String.valueOf(amount) + "\n";
             totalAmount += amount;
-
+            totalPoints += points;
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
+        result += "You earned " + String.valueOf(totalPoints)
                 + " frequent renter points";
         return result;
 
